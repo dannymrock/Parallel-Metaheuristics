b25a481df21f304a34d400e67f6371354dfc1d4e
Danny Munera <dannymunera@gmail.com>
2020-06-04 18:43:05 -0500

    upgrading code to python 3 and takking time on first examples

--- example1.py
+++ example1.py
@@ -1,15 +1,19 @@
 import random
 import time
+import numpy as np
 
 print("seed: ")
-seed = raw_input("Type your seed (none for using current time as seed): ")
-#print("input ", seed)
+seed = input("Type your seed (none for using current time as seed): ")
+
 if seed == '':
     seed = int(time.time())
+else:
+	seed = int(seed)
 
-random.seed(seed)
+print(seed)
+
+np.random.seed(seed)
 print("Seed:", seed)
 
 for x in range(10):
-    print random.randint(1,101)
-
+    print (np.random.randint(100))
--- example2.py
+++ example2.py
@@ -1,16 +1,16 @@
-import random
 import time
 import multiprocessing as mp
 import sys
+import numpy as np
+import queue
 
-ended_tasks=0
 
 def dummy_func (seed, output):
-    rand = random.Random(seed)
-    for i in xrange(1,1000000000):
-        x = 234.23423/i     
-    output.put(x)
-#========================================
+    print(seed)
+    np.random.seed(seed)
+    for i in range(1,1000000):                  # un gran ciclo
+        x = 234.2 / i * np.random.randint(100)  # una división en punto flotante     
+    output.put(x) 
 
 if __name__ == '__main__':
     # Get number of cores    
@@ -18,26 +18,51 @@ if __name__ == '__main__':
     print("Number of processors: ", cores)
 
     # Set random seed
-    seed = raw_input("Type a seed for your run (default: current system time): ")
+    print("seed: ")
+    seed = input("Type your seed (none for using current time as seed): ")
     if seed == '':
-        seed = int(time.time())   
-    random.seed(seed)
-    print("Seed:", seed)
+        seed = int(time.time())
+    else:
+        seed = int(seed)
+    print(seed)
+    np.random.seed(seed)
 
+
+    ### Sequential Execution: we want to execute dummy_func "cores" times
+    print("Starting sequential execution")
+
+    fifo_queue = queue.Queue()
+    start = time.time()         # taking initial time (from this line)
+    for i in range(cores):
+        dummy_func(np.random.randint(100), fifo_queue)
+    end = time.time()           # taking end time
+    # Printing results 
+    results = [fifo_queue.get() for i in range(cores)]
+    print(results)
+
+    print('Sequential execution time:', (end - start))
+
+
+    ### Parallel Execution
+    print("Starting parallel execution")
     #Initialize Pool
     output = mp.Queue()
 
     # Create parallel activities
     processes = [mp.Process(target=dummy_func,
-                            args=(random.randrange(sys.maxint),output))
+                            args=(np.random.randint(100),output))
                  for x in range(cores)]
 
+    start = time.time()         # taking initial time (from this line)
     for p in processes:
         p.start()
 
     for p in processes:
         p.join()
 
+    end = time.time()           # taking end time
     results = [output.get() for p in processes]
     print(results)
+
+    print('Parallel execution time:', (end - start))
 
